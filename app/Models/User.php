923809ec0f3d35eef7ef740a9e29d6d8b640ef0f
Daniel Galion <daniel.galion94@gmail.com>
2023-05-12 00:52:02 +0200

    Install permission manager - spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasRoles, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
